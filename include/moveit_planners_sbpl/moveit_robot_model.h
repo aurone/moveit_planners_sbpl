a40e4ddab303d19963c3146f91ead6c4ad71cf16
Andrew Dornbush <andrew.dornbush@gmail.com>
2016-06-06 13:24:47 -0400

    synchronize with sbpl_manipulation changes

--- include/moveit_planners_sbpl/moveit_robot_model.h
+++ include/moveit_planners_sbpl/moveit_robot_model.h
@@ -39,7 +39,7 @@
 #include <moveit/robot_state/robot_state.h>
 #include <moveit/planning_scene/planning_scene.h>
 #include <sbpl_arm_planner/robot_model.h>
-#include <sbpl_manipulation_components_pr2/orientation_solver.h>
+#include <sbpl_pr2_robot_model/orientation_solver.h>
 
 namespace sbpl_interface {
 
